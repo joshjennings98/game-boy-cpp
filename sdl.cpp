f065ad87c36a5b7ea4c7a3aa8dc673fe645f68cb
Josh <jenninjl01@gmail.com>
2020-08-05 13:11:41 +0100

    Made sdl stuff work

--- sdl.cpp
+++ sdl.cpp
@@ -1,174 +1,105 @@
 #include <iostream>
-#include "SDL2/SDL.h"
 #include "sdl.h"
 
-struct timeval t1, t2;
-SDL_Surface * screenX;
-
-Keypad::Keypad()
-{
-    Up = SDLK_w;
-    Down = SDLK_s;
-    Left = SDLK_a;
-    Right = SDLK_d;
-    A = SDLK_o;
-    B = SDLK_p;
-    Start = SDLK_u;
-    Select = SDLK_i;
-
-    keys = {Up, Down, Left, Right, A, B, Start, Select};
-
-    UpPressed = 0;
-    DownPressed = 0;
-    LeftPressed = 0;
-    RightPressed = 0;
-    APressed = 0;
-    BPressed = 0;
-    StartPressed = 0;
-    SelectPressed = 0;
-}
-
-uint8_t Keypad::getKey(int i)
-{
-    return keys[i];
-}
-
-Display::Display()
-{
-    for (int i = 0; i < 160; i++) {
-        for (int j = 0; j < 144; j++) {
-            display[i][j] = 0;
-        }
-    }
-}
-
-std::array<std::array<int, 144>, 160> Display::getDisplay()
-{
-    return display;
-}
-
-void Display::set(int x, int y, int pixel)
-{
-    display[x][y] = pixel;
-}
-
-int Display::get(int x, int y)
-{
-    return display[x][y];
-}
-
-void Display::drawDisplay(int scale, SDL_Renderer * renderer) 
-{
-    
-    SDL_Rect Message_Rect;
-    SDL_Surface* surface;
-    SDL_Texture* Message;
-
-    int pixel = 0;
-    
-    for (int i = 0; i < 160; i++) {
-        for (int j = 0; j < 144; j++) {
-            SDL_Rect r;
-            r.x = i * scale;
-            r.y = j * scale;
-            r.w = scale;
-            r.h = scale;
-
-            pixel = display[i][j];
-            
-            SDL_SetRenderDrawColor( renderer, pixel, pixel, pixel, 255 );
-            SDL_RenderFillRect( renderer, &r );
-        }
-    }
-    SDL_RenderPresent(renderer);
-}
-
-void Display::sdlSetFrame()
-{
-    if (frames == 0) {
-        gettimeofday(&t1, NULL);
-    }
-    frames++;
-    if (frames % 1000 == 0) { 
-        gettimeofday(&t2, NULL);
-        printf("FPS: %i\n", frames/((int)t2.tv_sec - (int)t1.tv_sec));
-    }
-    SDL_UpdateWindowSurface(screen);
-}
-
-void Display::sdlInit() {
+Display::Display() {
     SDL_Init(SDL_INIT_VIDEO);
-    this->screen = SDL_CreateWindow("Game Boy Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 160, 144, SDL_WINDOW_OPENGL); 
-    screenX = SDL_GetWindowSurface(this->screen);
-    frames = 0;
+    window = SDL_CreateWindow(
+        "Game Boy Emulator", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        160, 144, SDL_WINDOW_OPENGL
+    ); 
+    screen = SDL_GetWindowSurface(window);
 }
 
-unsigned int * Display::sdlFrameBuffer()
-{
-    return 0 ; //screen->pixels;
+void Display::sdlSetFrame(void) {
+    SDL_UpdateWindowSurface(window);
 }
 
-void Keypad::setPressed(int key, bool value) // {Up, Down, Left, Right, A, B, Start, Select}
-{
-    switch(key) {
-        case 0:
-            UpPressed = value;
-            break;
-        case 1:
-            DownPressed = value;
-            break;
-        case 2:
-            LeftPressed = value;
-            break;
-        case 3:
-            RightPressed = value;
-            break;
-        case 4:
-            APressed = value;
-            break;
-        case 5:
-            BPressed = value;
-            break;
-        case 6:
-            StartPressed = value;
-            break;
-        case 7:
-            SelectPressed = value;
-            break;
-    }
-}
-
-void Keypad::handleInput(SDL_Event e)
-{
-    while (SDL_PollEvent(&e)) {
-        // Handle key events
-        switch (e.type) {
-            case(SDL_KEYDOWN): 
-            {
-                if (e.key.keysym.sym == SDLK_ESCAPE) {
-                    exit(0);
-                }
-
-                for (int i = 0; i < 8; i++) {
-                    if (e.key.keysym.sym == getKey(i)) {
-                        setPressed(i, 1);
-                        std::cout << "Key " << i << " pressed." << std::endl;
-                    }
+int Display::sdlUpdate(void) {
+    SDL_Event event;
+    
+    while (SDL_PollEvent(&event)) {        
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+                    case SDLK_LEFT:
+                        buttons.left = 1;
+                        break;
+                    case SDLK_RIGHT:
+                        buttons.right = 1;
+                        break;
+                    case SDLK_UP:
+                        buttons.up = 1;
+                        break;
+                    case SDLK_DOWN:
+                        buttons.down = 1;
+                        break;
+                    case SDLK_z:
+                        buttons.a = 1;
+                        break;
+                    case SDLK_x:
+                        buttons.b = 1;
+                        break;
+                    case SDLK_a:
+                        buttons.start = 1;
+                        break;
+                    case SDLK_s:
+                        buttons.select = 1;
+                        break;
+                    default:
+                        break;
                 }
                 break;
-            }
-            case(SDL_KEYUP):
-            {
-                for (int i = 0; i < 8; i++) {
-                    if (e.key.keysym.sym == getKey(i)) {
-                        setPressed(i, 0);
-                        std::cout << "Key " << i << " released." << std::endl;
-                    }
+            case SDL_KEYUP:
+                switch(event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                    case SDLK_q:
+                        exit(0);
+                        break;
+                    case SDLK_LEFT:
+                        buttons.left = 0;
+                        break;
+                    case SDLK_RIGHT:
+                        buttons.right = 0;
+                        break;
+                    case SDLK_UP:
+                        buttons.up = 0;
+                        break;
+                    case SDLK_DOWN:
+                        buttons.down = 0;
+                        break;
+                    case SDLK_z:
+                        buttons.a = 0;
+                        break;
+                    case SDLK_x:
+                        buttons.b = 0;
+                        break;
+                    case SDLK_a:
+                        buttons.start = 0;
+                        break;
+                    case SDLK_s:
+                        buttons.select = 0;
+                        break;
+                    default:
+                        break;
                 }
                 break;
-            }
             default:
                 break;
         }
     }
+    return 0;
+}
+
+unsigned int * Display::sdlFrameBuffer() {
+    return static_cast<unsigned int*>(screen->pixels);
+}
+
+unsigned int Display::getButton(void) {
+    return ((buttons.start * 8) | (buttons.select * 4) | (buttons.b * 2) | buttons.a);
+}
+
+unsigned int Display::getDirection(void) {
+    return ((buttons.down * 8) | (buttons.up * 4) | (buttons.left * 2) | buttons.right);
 }
--- sdl.h
+++ sdl.h
@@ -1,59 +1,39 @@
 #ifndef SDL_H
 #define SDL_H
 
-#include <array>
-#include <iostream>
-#include "SDL2/SDL.h"
-#include <sys/time.h>
+#include <SDL2/SDL.h>
 
-class Keypad {
-    private:
-        uint8_t Up;
-        uint8_t Down;
-        uint8_t Left;
-        uint8_t Right;
-        uint8_t A;
-        uint8_t B;
-        uint8_t Start;
-        uint8_t Select;
-        
-        std::array<uint8_t, 8> keys;
+struct Buttons {
+    
+    int start;
+    int select;
 
-        bool UpPressed;
-        bool DownPressed;
-        bool LeftPressed;
-        bool RightPressed;
-        bool APressed;
-        bool BPressed;
-        bool StartPressed;
-        bool SelectPressed;
+    int a;
+    int b;
+    
+    int up;
+    int down;
+    int left;
+    int right;
 
-    public:
-        Keypad();
-        uint8_t getKey(int i);
-        void handleInput(SDL_Event e);
-        void setPressed(int key, bool value);
 };
 
 class Display {
     private:
-        std::array<std::array<int, 144>, 160> display;
+        SDL_Window * window;
+        SDL_Renderer * renderer;
+        SDL_Texture * texture;
+        SDL_Surface * screen;
+
+        Buttons buttons;
     public:
-        SDL_Window *screen;
-        SDL_Renderer *renderer;
-        SDL_Texture *texture;
-        SDL_Surface *surface;
-        unsigned int frames;
-
         Display();
-        void set(int x, int y, int pixel);
-        int get(int x, int y);
-        void drawDisplay(int scale, SDL_Renderer * renderer);
-        std::array<std::array<int, 144>, 160> getDisplay();
         void sdlSetFrame();
+        int sdlUpdate();
         unsigned int * sdlFrameBuffer();
-        void sdlInit();
-};
+        unsigned int getButton();
+        unsigned int getDirection();
 
+};
 
 #endif
