7a0f2cbfc6dd5c12fa4ee10f9af4b5efbf9d2450
Josh <jenninjl01@gmail.com>
2020-08-05 13:12:30 +0100

    Made it so new GameBoy class was used instead of defining everything in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,62 +10,15 @@
 #include "lcd.cpp"
 #include "sdl.cpp"
 #include "cpu.cpp"
+#include "gameboy.cpp"
 
 int main() {
 
-    unsigned int cycles = 0;
+    GameBoy gameboy(NoDebug);
 
-    Interrupts interrupts;
+    gameboy.load("tetris.gb");
 
-    LCD lcd = LCD(&interrupts, &cycles);
-
-    Timer timers = Timer(&interrupts, &cycles);
-
-    RAM ram = RAM(&timers, &interrupts, &lcd);
-
-    CPU cpu(&interrupts, &timers, &lcd, &ram, &cycles);
-
-    std::cout << std::endl;
-
-    cpu.loadROM("tetris.gb");
-
-    // test crap
-    ram.dump(0x0000, 0x0020);
-    /*
-
-    Keypad keypad = Keypad();
-    Display display = Display();
-
-    int scale = 8;
-    int width = scale * 160, height = scale * 144;
-
-    SDL_Event e;
-
-    SDL_Window* window = SDL_CreateWindow ("Gameboy Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);;
-    SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-    SDL_RenderPresent(renderer);
-
-    //while (true) {
-    for (int i = 0; i < 500; i++) {
-               
-        
-        // Get rid of
-        for (int i = 0; i < 160; i++) {
-            for (int j = 0; j < 144; j++) {
-                display.set(i, j, (display.get(i, j) + 1) % 255);
-            }
-        }
-
-        display.drawDisplay(scale, renderer);
-        keypad.handleInput(e);
-
-        // Slow down emulation speed
-        std::this_thread::sleep_for(std::chrono::microseconds(1200));
-
-        //std::cout << "cycle " << i << std::endl;
-    }
-    */
+    gameboy.run();
 
     return 0;
 }
